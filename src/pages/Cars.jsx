2f8dbd2e7a55f5b2fc88946775121eebe4fdb1de
Shivam Sutar <shivamsutar33@hotmail.com>
2024-12-25 14:24:25 +0530

    css fixed for cars page

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -20,7 +20,7 @@ const Cars = () => {
   };
   return (
     <section className="pt-20 px-6 py-6">
-      <section className="grid grid-cols-12 gap-10">
+      <section className="grid grid-cols-12 gap-y-5 sm:gap-10">
         {isLoading
           ? getCarsLoadingComponent()
           : data?.length > 0
